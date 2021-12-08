e308275e4001e07896a2a9b5c56e339c1b891b44
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-12-08 13:55:01 -0500

    mock con spyOn y override

--- src/math.js
+++ src/math.js
@@ -2,3 +2,10 @@ export const add      = (a, b) => a + b;
 export const subtract = (a, b) => b - a;
 export const multiply = (a, b) => a * b;
 export const divide   = (a, b) => b / a;
+ 
+//asi se veria cada uno de los mocks
+
+// export const add      = jest.fn();
+// export const subtract = jest.fn();
+// export const multiply = jest.fn();
+// export const divide   = jest.fn();
---
+++ test/mock_jest_spyOn_sugar.test.js
@@ -0,0 +1,26 @@
+import * as app from '../src/App'
+import * as math from '../src/math'
+
+//como hicimos anteriormente vamos a aplicar mock con SpyOn 
+test("calls math.add", () => {
+
+  // almacenamos en la varible originalAdd la implementacion Add
+  const originalAdd = math.add;
+
+  // mockeamos con la implementacion original tal como lo anteriormente visto
+  math.add = jest.fn(originalAdd);
+
+  // espiamos las llamadas de la implementacion
+  expect(app.doAdd(1, 2)).toEqual(3);
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+
+  // hacemos un override a la implementacion
+  math.add.mockImplementation(() => "mock");
+  expect(app.doAdd(1, 2)).toEqual("mock");
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+  
+  // restauramos a la implementacion original
+  math.add = originalAdd;
+  expect(app.doAdd(1, 2)).toEqual(3);
+
+});
